a80a01d22d49e4eb113a1ed427d6514b4aa8e6b9
Maria Vitória <108490423+maria-vitoria03@users.noreply.github.com>
2024-03-08 01:30:24 +0000

    Novas alterações no código

--- main.c
+++ main.c
@@ -27,59 +27,70 @@ int main(void) {
     getchar(); // Limpar o buffer de entrada.
 
     // Menu do aluno
-    switch (menu) {
+    if (menu < 0 || menu > 4) { // Condição caso o usuário informe um valor
+                                // diferente dos fornecidos no menu
+      printf("Condição Inválida! Tente novamente.\n");
+    } else {
+      switch (menu) {
 
-    case 0: {
-      sair = 1;
-      break;
-    }
-    case 1: {
-      printf("----------------------------\n");
-      printf(" ***** Pasta de alunos ***** \n");
-      int opcaoAluno;
+      case 0: {
+        sair = 1;
+        break;
+      }
+      case 1: {
+        printf("----------------------------\n");
+        printf(" ***** Pasta de alunos ***** \n");
+        int opcaoAluno;
 
-      while (!sairAluno) {
-        printf("0 - Voltar\n");
-        printf("1 - Cadastrar Aluno\n");
-        printf("2 - Listar Aluno\n");
-        printf("3 - Atualizar Aluno\n");
-        printf("4 - Excluir Aluno\n");
-        printf("Informe:");
+        while (!sairAluno) {
+          printf("0 - Voltar\n");
+          printf("1 - Cadastrar Aluno\n");
+          printf("2 - Listar Aluno\n");
+          printf("3 - Atualizar Aluno\n");
+          printf("4 - Excluir Aluno\n");
+          printf("Informe:");
 
-        scanf("%d", &opcaoAluno);
-        getchar(); // Limpar o buffer de entrada.
+          scanf("%d", &opcaoAluno);
+          getchar(); // Limpar o buffer de entrada.
 
-        // subMenu
-        switch (opcaoAluno) {
-        case 0: {
-          sairAluno = 1;
-          break;
-        }
-        case 1: {
-          printf("---------------\n");
-          printf("Cadastrar Aluno\n");
-          printf("Digite a matrícula:");
-          int matricula;
-          scanf("%d", &matricula);
-          if (matricula < 0) {
-            printf("Matrícula Inválida!\n");
+          if (opcaoAluno < 0 ||
+              opcaoAluno > 4) { // Condição caso o usuário informe um valor
+                                // diferente dos fornecidos no menu.
+            printf("Condição Inválida! Tente novamente.\n");
           } else {
-            if (qtdAluno == TAM_ALUNO) {
-              printf("Lista de alunos cheia!!\n");
-            } else {
-              listaAluno[qtdAluno] = matricula;
-              qtdAluno++;
+            // subMenu
+            switch (opcaoAluno) {
+            case 0: {
+              sairAluno = 1;
+              break;
+            }
+            case 1: {
+              printf("---------------\n");
+              printf("Cadastrar Aluno\n");
+              int matricula;
+              if (qtdAluno == TAM_ALUNO) {
+                printf("Lista de alunos cheia!!\n");
+              } else {
+                printf("Digite a matrícula:\n");
+                scanf("%d", &matricula);
+                if (matricula < 0) {
+                  printf("Matrícula Inválida!\n");
+                } else {
+                  listaAluno[qtdAluno] = matricula;
+                  qtdAluno++;
+                  printf("Aluno cadastrado com sucesso!!\n");
+                }
+              }
+              break;
+            }
             }
           }
-          break;
         }
-        }
+        sairAluno = 0;
+        break;
       }
-      sairAluno = 0;
-      break;
-    }
+      }
     }
   }
-
   return 0;
 }
